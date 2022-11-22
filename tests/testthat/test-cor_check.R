36062dab0b3116f9768414d3c85588b848d85318
SherryLi1234 <sherli@umich.edu>
2022-11-22 15:57:37 -0500

    cor_check test cases

--- tests/testthat/test-cor_check.R
+++ tests/testthat/test-cor_check.R
@@ -1,5 +1,6 @@
 test_that("multiplication works", {
   expect_equal(all(grepl("Pass", cor_check(mtcars$mpg,mtcars$cyl))), T)
   expect_equal(all(grepl("Pass", cor_check(mtcars$mpg,mtcars$drat))), T)
+  expect_equal(any(grepl("Pass", cor_check(c(1,2,3,"4",""),c(2,3,4))))==FALSE, FALSE)
 })
 
